13e9e780c034c165c427b3657a0a0a3a98483341
Nishit Sarvaiya <nishitsarvaiiya@gmail.com>
2023-10-01 16:26:46 +0530

    minor change

--- src/app/StoreSection.jsx
+++ src/app/StoreSection.jsx
@@ -1,28 +1,29 @@
-'use client';
+// 'use client';
 
-import { Environment, OrbitControls, useTexture } from '@react-three/drei';
-import { Canvas } from '@react-three/fiber';
 import Image from 'next/image';
-import { Suspense } from 'react';
-import { BackSide } from 'three';
+
+// import { OrbitControls, useTexture } from '@react-three/drei';
+// import { Canvas } from '@react-three/fiber';
+// import { Suspense } from 'react';
+// import { BackSide } from 'three';
 
 export default function StoreSection() {
 	return (
-		<section className='store-section'>
+		<section className='store-section' data-scroll-section>
 			<div className='container'>
 				<div className='store-wrapper'>
-					<div className='store-wrapper--inner'>
-						{/* <Image
+					<div className='store-wrapper--inner' data-scroll data-scroll-speed='-0.1'>
+						<Image
 							src='https://images.unsplash.com/photo-1450297166380-cabe503887e5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1920&q=80'
 							alt=''
 							fill
 							style={{ objectFit: 'cover' }}
-						/> */}
-						<Canvas camera={{ position: [0, 0, -2] }}>
+						/>
+						{/* <Canvas camera={{ position: [0, 0, -2] }}>
 							<Suspense fallback={null}>
 								<Scene />
 							</Suspense>
-						</Canvas>
+						</Canvas> */}
 					</div>
 				</div>
 			</div>
@@ -30,18 +31,18 @@ export default function StoreSection() {
 	);
 }
 
-const Scene = () => {
-	const texture = useTexture('/images/store.jpg');
+// const Scene = () => {
+// 	const texture = useTexture('/images/store.jpg');
 
-	return (
-		<>
-			<ambientLight intensity={3} />
-			<OrbitControls enableZoom={false} enablePan={false} reverseOrbit={true} />
+// 	return (
+// 		<>
+// 			<ambientLight intensity={3} />
+// 			<OrbitControls enableZoom={false} enablePan={false} reverseOrbit={true} />
 
-			<mesh>
-				<sphereGeometry args={[5, 64, 64]} />
-				<meshStandardMaterial map={texture} side={BackSide} />
-			</mesh>
-		</>
-	);
-};
+// 			<mesh>
+// 				<sphereGeometry args={[5, 64, 64]} />
+// 				<meshStandardMaterial map={texture} side={BackSide} />
+// 			</mesh>
+// 		</>
+// 	);
+// };
